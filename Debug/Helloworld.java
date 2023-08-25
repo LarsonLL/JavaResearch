1d0a7729b9d9f13f7e4a0e33abcb5cc1f07b3ce9
larson liang <larson54264@outlook.com>
2023-08-25 23:17:37 +0800

    test

---
+++ Debug/Helloworld.java
@@ -0,0 +1,29 @@
+package Debug;
+
+//import Math;
+
+
+public class Helloworld {
+	public static void main(String args[]) {
+		System.out.println("Hello World!");
+		Math.sin(0);
+		System.out.println(Math.sin(0));
+		Math.round(6.6);  //四舍五入取整，返回7
+		Math.sqrt(144);  //开平方函数，返回12.0
+		Math.pow(5,2);  //计算乘方，返回25.0
+		Math.max(560,289);  //最大值函数 返回560
+		Math.min(560,289);  //最小值函数，返回289
+		Math.random();  //返回0.0 到 1.0之间双精度的一个随机数。
+		//算数运算符
+		int i = 3;
+		++i;
+		System.out.println(i);
+		++i;
+		System.out.println(i);
+
+
+		
+
+	}
+
+}
