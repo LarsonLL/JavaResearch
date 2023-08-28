c94556ec33425d5bdf6140c0077b29f76dcdd077
larson liang <larson54264@outlook.com>
2023-08-28 23:23:28 +0800

    1

--- JavaLearn/Chapter3/SwitchExample.java
+++ JavaLearn/Chapter3/SwitchExample.java
@@ -5,22 +5,23 @@ public class SwitchExample{
         switch (ch) {
             case '0': case '1': case '2': case '3': case '4': case '5':
             case '6': case '7': case '8': case '9':
-                System.out.println("the first character is digit" + ch);
+                System.out.println("the first character is digit " + ch);
                 break;
             case 'a': case 'b': case 'c': case 'd': case 'e': case 'f':
             case 'g': case 'h': case 'i': case 'j': case 'k': case 'l':
             case 'm': case 'n': case 'o': case 'p': case 'q': case 'r':
             case 's': case 't': case 'u': case 'v': case 'w': case 'x':
             case 'y': case 'z':
-                System.out.println("the first character is letter" + ch);
+                System.out.println("the first character is letter " + ch);
                 break;
 
             case 'A': case 'B': case 'C': case 'D': case 'E': case 'F':
             case 'G': case 'H': case 'I': case 'J': case 'K': case 'L':
+            
             case 'M': case 'N': case 'O': case 'P': case 'Q': case 'R':
             case 'S': case 'T': case 'U': case 'V': case 'W': case 'X':
             case 'Y': case 'Z':
-                System.out.println("the first character is uppercase letter" + ch);
+                System.out.println("the first character is uppercase letter " + ch);
                 break;
             default:
                 System.out.println("the first character [" + ch+"] is neither a letter nor a digit");
